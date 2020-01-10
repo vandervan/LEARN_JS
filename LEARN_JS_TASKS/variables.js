021a3d18da5c334be8c310801a2dae5e2ff896ac
Arthur Krivitsky <arthur.vandervan@gmail.com>
2020-01-10 16:45:18 +0300

    bug fixes

--- LEARN_JS_TASKS/variables.js
+++ LEARN_JS_TASKS/variables.js
@@ -8,14 +8,14 @@ console.log(a);
 let q, qwe, name; //в JavaScript можно объявлять переменные в одну строку. Как и комментарии. Небольшие
 q = 11;
 qwe = 'privet';
-name = Valera;
+name = 'Valera';
 console.log(q, qwe, name);
 
 
 /*хорошим практисом считается CamelCase сложносоставных имен переменных
 */
 var planetEarth = '123'; 
-console.log(b);
+console.log(planetEarth);
 
 /*жесткая константа, которую не будем вычислять позже
 */
@@ -26,5 +26,5 @@ console.log(BIRTHDAY);
 /*менее "константная константа", которая будет использоваться в вычислениях, 
 либо других методах, как например someMethod
 */
-const age = someMethod(BIRTHDAY); 
+const age = typeof BIRTHDAY;
 console.log(age);
