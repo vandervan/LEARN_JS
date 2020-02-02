ec84a2dd69982a7ae957a5b95ac25d0c4372704c
vandervan <arthur.vandervan@gmail.com>
2020-02-02 17:40:14 +0300

    Closures homework

--- LEARN_JS_TASKS/Closure.js
+++ LEARN_JS_TASKS/Closure.js
@@ -32,6 +32,22 @@ alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
 alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
 
  */
+function inBetween(a, b) {
+  return function (x) {
+    return x >= a && x <= b;
+  };
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr.filter(inBetween(3, 6))); // 3,4,5,6
+
+function inArray(arr) {
+  return function (x) {
+    return arr.includes(x);
+  };
+}
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr.filter(inArray([1, 2, 10]))); // 1,2
 
 /* Задача 3. Сортировать по полю
 У нас есть массив объектов, который нужно отсортировать:
@@ -57,6 +73,15 @@ users.sort(byField('age'));
 Напишите функцию byField, которая может быть использована для этого.
 
  */
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+function byFiled(fieldName) {
+  return (a, b) => a[fieldName] > b[fieldName] ? 1 : -1;
+}
 
 
 /* Задача 4. Армия функций
