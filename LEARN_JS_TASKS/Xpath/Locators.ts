5c630da3b830dcfbc4ecbc33acf8e2f58b07c8e5
vandervan <arthur.vandervan@gmail.com>
2020-02-09 16:38:46 +0300

    Locators_homework

---
+++ LEARN_JS_TASKS/Xpath/Locators.ts
@@ -0,0 +1,22 @@
+BBC.com
+
+search_button_Xpath        //input[@id='orb-search-q']
+CSS                         #orb - search - q
+
+weather_button_Xpath       //div[@class='orb-panel-content b-g-p b-r orb-nav-sec']//a[contains(text(),'Weather')]
+CSS                         div.orb - nav - pri.orb - nav - pri - white.orb - nav - dyn.orb - nav - active div.orb - panel - active div.orb - panel div.orb - panel - content.b - g - p.b - r.orb - nav - sec ul: nth - child(2) li.orb - nav - weather > a: nth - child(1)
+
+main_block_Xpath           //a[@class='block-link__overlay-link'][contains(text(),'Thai soldier kills 20 in shooting rampage')]
+CSS                         div.content: nth - child(2) section.module.module--promo: nth - child(3) div.module__content ul.media - list li.media - list__item.media - list__item--1: nth - child(1) div.media.media--hero.media--primary.media--overlay.block - link > a.block - link__overlay - link
+
+even_button_2_Xpath        //div[@id='orb-nav-links']//a[contains(text(),'Sport')]
+CSS                         div.orb - nav - pri.orb - nav - pri - white.orb - nav - dyn.orb - nav - active div.orb - nav - pri - container.b - r.b - g - p nav.orb - nav: nth - child(4) div.orb - nav - section.orb - nav - links.orb - nav - focus ul: nth - child(1) li.orb - nav - sport > a: nth - child(1)
+
+even_button_4_Xpath        //div[@id='orb-nav-links']//a[contains(text(),'Worklife')]
+CSS                         div.orb - nav - pri.orb - nav - pri - white.orb - nav - dyn.orb - nav - active div.orb - nav - pri - container.b - r.b - g - p nav.orb - nav: nth - child(4) div.orb - nav - section.orb - nav - links.orb - nav - focus ul: nth - child(1) li.orb - nav - worklife > a: nth - child(1)
+
+even_button_6_Xpath        //div[@id='orb-nav-links']//a[contains(text(),'Future')]
+CSS                         div.orb - nav - pri.orb - nav - pri - white.orb - nav - dyn.orb - nav - active div.orb - nav - pri - container.b - r.b - g - p nav.orb - nav: nth - child(4) div.orb - nav - section.orb - nav - links.orb - nav - focus ul: nth - child(1) li.orb - nav - future > a: nth - child(1)
+
+date_Xpath                 //h2[contains(text(),'Sunday, 9 February')]
+CSS                         body: nth - child(2) div: nth - child(13) div.content: nth - child(2) section.module.module--header: nth - child(2) > h2.module__title
