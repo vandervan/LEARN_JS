a3372ed88606f93053b4dd701999db4bf7a9a296
Arthur Krivitsky <arthur.vandervan@gmail.com>
2020-02-18 20:37:53 +0300

    Custom waiters v.1.0

--- LEARN_JS_TASKS/Selenium/BBC_Firefox_test.js
+++ LEARN_JS_TASKS/Selenium/BBC_Firefox_test.js
@@ -1,20 +1,34 @@
-const {Builder, By, Key,} = require('selenium-webdriver');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function example() {
     const driver = await new Builder().forBrowser('firefox').build();
-    try {
         await driver.get('https://www.bbc.com');
-        await driver.findElement(By.css('#orb-search-q')).sendKeys('coronavirus', Key.RETURN);
-        const isDisplay = await driver.findElement(By.id('se-searchbox-input-field')).isDisplayed();
-        console.log(isDisplay);
-        const isEnable = await driver.findElement(By.id('se-searchbox-input-field')).isEnabled();
-        console.log(isEnable);
-        await driver.findElement(By.css('[href="https://www.bbc.com/news"]')).click();
-        await driver.sleep(5000);
+        const searchField = await driver.findElement(By.id('orb-search-q'));
 
-    } finally {
+            await driver.wait(function () {
+                return searchField.isEnabled();
+            }, 15000);
+
+        await searchField.sendKeys('coronavirus', Key.RETURN);
+
+        const searchField2 = await driver.findElement(By.id('q'));
+            await driver.wait(function () {
+                return searchField2.isEnabled();
+             }, 15000);
+
+        const newsButton = await driver.findElement(By.css('[href="https://www.bbc.com/news"]'));
+
+            await driver.wait(function () {
+                return newsButton.isEnabled();
+            }, 15000);
+        await newsButton.click();
+
         await driver.quit();
-    }
 })();
 
+
+// implicitWait - Driver - неявные - noSuchElementException
+// explicitWait - Selenium Library - явные - timeoutException
+//isDisplayed() -- TRUE if isPresent() --true + element is visible
+// - Fail; isEnabled() - OK; isPresent() - Fail;
 
