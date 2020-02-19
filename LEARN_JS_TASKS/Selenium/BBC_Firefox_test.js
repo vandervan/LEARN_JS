c160531ff8c52c11c615780a0abef02c670d7f2a
Arthur Krivitsky <arthur.vandervan@gmail.com>
2020-02-19 16:29:10 +0300

    Fix PR

--- LEARN_JS_TASKS/Selenium/BBC_Firefox_test.js
+++ LEARN_JS_TASKS/Selenium/BBC_Firefox_test.js
@@ -1,27 +1,14 @@
-function waitForEnabled(element) {
-    if (element.isEnabled()) {
-        return element.click();
-    } else {
-        return  waitForEnabled();
-    }
-}
-
-function waitForDisplayed(element) {
-    if (element.isDisplayed()) {
-       return element.sendKeys('coronavirus', Key.RETURN);
-    } else {
-       return waitForDisplayed();
-    }
-}
-
-const {Builder, By, Key} = require('selenium-webdriver');
-
 (async function BBC_FIREFOX() {
-    const driver = await new Builder().forBrowser('firefox').build();
+    const {Builder, By, Key} = require('selenium-webdriver');
+    const driver = new Builder().forBrowser('firefox').build();
         await driver.get('https://www.bbc.com');
-        const searchField = await driver.findElement(By.id('orb-search-q'));
-        await waitForDisplayed(searchField);
-        const newsButton = await driver.findElement(By.css('[href="https://www.bbc.com/news"]'));
-        await waitForEnabled(newsButton);
+        await driver.wait(function waitForEnabled() {
+            return driver.findElement(By.id('orb-search-q')).isEnabled();
+        }, 10000);
+        await driver.findElement(By.id('orb-search-q')).sendKeys('coronavirus', Key.RETURN);
+        await driver.wait(function waitForDisplayed() {
+            return driver.findElement(By.css('[href="https://www.bbc.com/news"]')).isDisplayed();
+        }, 10000);
+        await driver.findElement(By.css('[href="https://www.bbc.com/news"]')).click();
         await driver.quit();
 })();
