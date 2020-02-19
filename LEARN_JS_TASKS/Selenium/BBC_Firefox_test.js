1379c8599758b5efd32467f25c84d66703c92471
vandervan <arthur.vandervan@gmail.com>
2020-02-19 09:42:10 +0300

    custom conditions

--- LEARN_JS_TASKS/Selenium/BBC_Firefox_test.js
+++ LEARN_JS_TASKS/Selenium/BBC_Firefox_test.js
@@ -1,27 +1,24 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {Builder, By, Key} = require('selenium-webdriver');
 
 (async function example() {
     const driver = await new Builder().forBrowser('firefox').build();
         await driver.get('https://www.bbc.com');
         const searchField = await driver.findElement(By.id('orb-search-q'));
-
-            await driver.wait(function () {
-                return searchField.isEnabled();
+             driver.wait(function () {
+                return searchField.isDisplayed();
             }, 15000);
+            await searchField.sendKeys('coronavirus', Key.RETURN);
 
-        await searchField.sendKeys('coronavirus', Key.RETURN);
-
-        const searchField2 = await driver.findElement(By.id('q'));
-            await driver.wait(function () {
-                return searchField2.isEnabled();
+        const searchField2 = await driver.findElement(By.css('#se-searchbox-input-field'));
+             driver.wait(function () {
+                return searchField2.isDisplayed();
              }, 15000);
 
         const newsButton = await driver.findElement(By.css('[href="https://www.bbc.com/news"]'));
-
-            await driver.wait(function () {
-                return newsButton.isEnabled();
+             driver.wait(function () {
+                return newsButton.isDisplayed();
             }, 15000);
-        await newsButton.click();
+            await newsButton.click();
 
         await driver.quit();
 })();
