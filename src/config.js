7418d2ca96250e3c6281c6582ae8814fcc70708b
Arthur Krivitsky <arthur.vandervan@gmail.com>
2020-03-24 13:00:46 +0300

    Jenkins test

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const chrome = require('selenium-webdriver/chrome');
 const options = new chrome.Options();
 exports.config = {
     framework: 'jasmine2',
-    //seleniumAddress: 'http://localhost:4444/wd/hub',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
     SELENIUM_PROMISE_MANAGER: true,
     directConnect: true,
     capabilities: {
